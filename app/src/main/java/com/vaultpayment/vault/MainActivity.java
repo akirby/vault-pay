3587c83fa8ba261609e2c2137828d338f9456adc
akirby <andrew.m.kirby50@gmail.com>
2019-03-12 20:25:13 -0400

    update gui to add some of the styling from zeplin

--- app/src/main/java/com/vaultpayment/vault/MainActivity.java
+++ app/src/main/java/com/vaultpayment/vault/MainActivity.java
@@ -10,12 +10,16 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    private ImageView signal;
+    private TextView cardNumber;
     private Button btnTestMain;
 
 
@@ -32,26 +36,48 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     btnAdd.hide();
+                    signal.setVisibility(View.VISIBLE);
                     paymentdesc.setText(R.string.pay_desc);
+                    paymentdesc.setVisibility(View.VISIBLE);
+                    cardNumber.setVisibility(View.VISIBLE);
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.payment_methods_title);
                     btnAdd.show();
+                    signal.setVisibility(View.INVISIBLE);
                     paymentdesc.setText("");
+                    paymentdesc.setVisibility(View.INVISIBLE);
+                    cardNumber.setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_history);
                     btnAdd.hide();
+                    signal.setVisibility(View.INVISIBLE);
                     paymentdesc.setText("");
+                    paymentdesc.setVisibility(View.INVISIBLE);
+                    cardNumber.setVisibility(View.INVISIBLE);
                     return true;
             }
             return false;
         }
     };
 
+    public void SignalActive(boolean isActive){
+        if(isActive) {
+            signal.setColorFilter(R.color.neon_blue);
+        }
+        else{
+            signal.setColorFilter(R.color.light_grey_n_f_c_near);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getSupportActionBar().setTitle("");
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setLogo(R.mipmap.vault_logo);
+        //getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_main);
         AppCompatActivity thisActivity = this;
         if (ContextCompat.checkSelfPermission(thisActivity, Manifest.permission.NFC)
@@ -66,6 +92,9 @@ public class MainActivity extends AppCompatActivity {
         btnAdd.hide();
         paymentdesc = (TextView) findViewById(R.id.payscreendescription);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        signal = (ImageView) findViewById(R.id.signal);
+        cardNumber = (TextView) findViewById(R.id.card_number);
 
     }
 
